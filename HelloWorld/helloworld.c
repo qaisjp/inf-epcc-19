16cf4bb9096de967a782255728a889f4265b1d4c
Qais Patankar <qaisjp@gmail.com>
2019-09-27 05:18:41 +0100

    reformat helloworld

--- HelloWorld/helloworld.c
+++ HelloWorld/helloworld.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int main(){
+int main()
+{
 #pragma omp parallel
     {
-#pragma omp critical 
-     {
-      printf("hello from thread %d\n",omp_get_thread_num());
-     }
+#pragma omp critical
+        {
+            printf("hello from thread %d\n", omp_get_thread_num());
+        }
     }
 }
