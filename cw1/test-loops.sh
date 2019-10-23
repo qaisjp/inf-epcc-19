f344117f4d82293e07b4676bbd76a01d868ce44c
Qais Patankar <qaisjp@gmail.com>
2019-10-23 22:14:01 +0100

    cw1: improve tests

--- cw1/test-loops.sh
+++ cw1/test-loops.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+
+# Colors
+GREY='\033[0;37m' # Well, light grey.
+RED='\033[1;31m' # It's actually light red
+NC='\033[0m' # No Color
+CYAN='\033[0;36m'
+
 OUTPUT_FILE=`mktemp`
 ./loops > $OUTPUT_FILE
-echo $OUTPUT_FILE
+
 SUCCESS_OK=1
-if ! grep "Loop 1 check: Sum of a is -343021.474766" $OUTPUT_FILE; then
-    echo "FAILED LOOP 1:"
+if ! grep -q "Loop 1 check: Sum of a is -343021.474766" $OUTPUT_FILE; then
+    echo -e "${GREY}FAILED LOOP 1:${NC}"
     grep "Loop 1 check: Sum of a is" $OUTPUT_FILE
     SUCCESS_OK=0
 fi
 
-if ! grep "Loop 2 check: Sum of c is -25242644.603147" $OUTPUT_FILE; then
-    echo "FAILED LOOP 2:"
+if ! grep -q "Loop 2 check: Sum of c is -25242644.603147" $OUTPUT_FILE; then
+    echo -e "${GREY}FAILED LOOP 2:${NC}"
     grep "Loop 2 check: Sum of c is" $OUTPUT_FILE
     SUCCESS_OK=0
 fi
 
 if [ $SUCCESS_OK -eq "0" ]; then
-    echo "FAILED"
+    echo -e "${RED}FAILED${NC}"
 else
-    echo "SUCCESS"
+    echo -e "${CYAN}SUCCESS${NC}"
 fi
 
+echo ""
+cat $OUTPUT_FILE
 rm $OUTPUT_FILE
