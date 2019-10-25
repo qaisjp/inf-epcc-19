5284d69c81ade54e73b0deef81c921b18fad342c
Qais Patankar <qaisjp@gmail.com>
2019-10-25 02:24:18 +0100

    cw1: actually more efficient to run more jobs

--- cw1/qwik.sh
+++ cw1/qwik.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-for n in 1 2 4 8 16 32 64; do
+# Colors
+GREY='\033[0;37m' # Well, light grey.
+RED='\033[1;31m' # It's actually light red
+NC='\033[0m' # No Color
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+
+make test
+for THREADS in 01 02 04 06 08 12 16; do
+    export THREADS
     echo ""
     echo ""
-    export OMP_SCHEDULE="$1,$n"
-    export OMP_NUM_THREADS=4
-    echo $OMP_SCHEDULE
-    make deploy
+    for NUM in {00..99}; do
+        export OMP_SCHEDULE="static,64"
+        FILENAME="cw1-thread-$THREADS-n$NUM"
+        echo "$FILENAME - sched: ${OMP_SCHEDULE}"
+        if ! qsub threadloops.pbs -o "${FILENAME}.stdout.txt" -e "${FILENAME}.stderr.txt" -V; then
+            echo -e "${RED}qsub was not happy!${NC}"
+            exit 1
+        fi
+    done
     echo ""
     echo ""
 done
